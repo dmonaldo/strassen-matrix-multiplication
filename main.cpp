6a94abdf52a3e294f6a95fe35c3360d0606b7ad1
Dante Monaldo <dante.monaldo@gmail.com>
2020-02-08 15:39:18 -0800

    Populate matrices from input file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+const string INPUT_FILE_NAME = "input.txt";
+
 int main() {
-    cout << "Hello World\n";
+    int matrixSize;
+    ifstream inputFile;
+    inputFile.open(INPUT_FILE_NAME);
+
+    // get the size of the matrix from first line of input file
+    if (inputFile.is_open()) {
+        string firstLine;
+        getline(inputFile, firstLine);
+        matrixSize = stoi(firstLine);
+    }
+
+    // initialize matrices
+    int matrixA[matrixSize*matrixSize];
+    int matrixB[matrixSize*matrixSize];
+
+    // populate matrices with values from input file
+    if (inputFile.is_open()) {
+        int lineCounter = 0;
+        int i = 0;
+        while (!inputFile.eof()) {
+            if (lineCounter < matrixSize * matrixSize) {
+                inputFile >> matrixA[i];
+                i++;
+            } else if (lineCounter == matrixSize * matrixSize) {
+                i = 0;
+            } else {
+                inputFile >> matrixB[i];
+                i++;
+            }
+            lineCounter++;
+        }
+    }
+
+    inputFile.close();
+
     return 0;
 }
