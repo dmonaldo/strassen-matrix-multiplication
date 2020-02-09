14f6d48cacbc12e53805081cc4cb542de61f7668
Dante Monaldo <dante.monaldo@gmail.com>
2020-02-08 17:16:50 -0800

    Committing now, going to convert array to vectors

--- main.cpp
+++ main.cpp
@@ -1,33 +1,62 @@
 #include <iostream>
 #include <fstream>
+#include <typeinfo> 
 using namespace std;
 
 const string INPUT_FILE_NAME = "input.txt";
 
 //
 void strassenR(int *&matrixA, int *&matrixB, int *&matrixC) {
-    int P[7];
+    int arraySize = sizeof(matrixA) / sizeof(matrixA[0]);
 
-    P[0] = (matrixA[0] * matrixB[1]) - (matrixA[0] * matrixB[3]);
-    P[1] = (matrixA[0] * matrixB[3]) + (matrixA[1] * matrixB[3]);
-    P[2] = (matrixA[2] * matrixB[0]) + (matrixA[3] * matrixB[0]);
-    P[3] = (matrixA[3] * matrixB[2]) - (matrixA[3] * matrixB[0]);
-    P[4] = (matrixA[0] * matrixB[0]) + (matrixA[0] * matrixB[3]) + (matrixA[3] * matrixB[0]) + (matrixA[3] * matrixB[3]);
-    P[5] = (matrixA[1] * matrixB[2]) + (matrixA[1] * matrixB[3]) - (matrixA[3] * matrixB[2]) - (matrixA[3] * matrixB[3]);
-    P[6] = (matrixA[0] * matrixB[0]) + (matrixA[0] * matrixB[1]) - (matrixA[2] * matrixB[0]) - (matrixA[2] * matrixB[1]);
+    if (arraySize == 2) {
+        cout << "yee";
+    } else {
+        int S[10];
 
-    matrixC[0] = P[4] + P[3] - P[1] + P[5];
-    matrixC[1] = P[0] + P[1];
-    matrixC[2] = P[2] + P[3];
-    matrixC[3] =  P[4] + P[0] - P[2] - P[6];
+        S[0] = matrixB[1] - matrixB[3];
+        S[1] = matrixA[0] + matrixA[1];
+        S[2] = matrixA[2] + matrixA[3];
+        S[3] = matrixB[2] - matrixB[0];
+        S[4] = matrixA[0] + matrixA[3];
+        S[5] = matrixB[0] + matrixB[3];
+        S[6] = matrixA[1] - matrixA[3];
+        S[7] = matrixB[2] + matrixB[3];
+        S[8] = matrixA[0] - matrixA[2];
+        S[9] = matrixB[0] + matrixB[1];
 
-    for (int i = 0; i < 7; i++) {
-        cout << P[i] << endl;
+        int P[7];
+        // P[0] = (matrixA[0] * matrixB[1]) - (matrixA[0] * matrixB[3]);
+        // P[1] = (matrixA[0] * matrixB[3]) + (matrixA[1] * matrixB[3]);
+        // P[2] = (matrixA[2] * matrixB[0]) + (matrixA[3] * matrixB[0]);
+        // P[3] = (matrixA[3] * matrixB[2]) - (matrixA[3] * matrixB[0]);
+        // P[4] = (matrixA[0] * matrixB[0]) + (matrixA[0] * matrixB[3]) + (matrixA[3] * matrixB[0]) + (matrixA[3] * matrixB[3]);
+        // P[5] = (matrixA[1] * matrixB[2]) + (matrixA[1] * matrixB[3]) - (matrixA[3] * matrixB[2]) - (matrixA[3] * matrixB[3]);
+        // P[6] = (matrixA[0] * matrixB[0]) + (matrixA[0] * matrixB[1]) - (matrixA[2] * matrixB[0]) - (matrixA[2] * matrixB[1]);
+
+        P[0] = matrixA[0] * S[0];
+        P[1] = S[1] * matrixB[3];
+        P[2] = S[2] * matrixB[0];
+        P[3] = matrixA[3] * S[3];
+        P[4] = S[4] * S[5];
+        P[5] = S[6] * S[7];
+        P[6] = S[8] * S[9];
+
+
+        matrixC[0] = P[4] + P[3] - P[1] + P[5];
+        matrixC[1] = P[0] + P[1];
+        matrixC[2] = P[2] + P[3];
+        matrixC[3] =  P[4] + P[0] - P[2] - P[6];
+
+        for (int i = 0; i < 7; i++) {
+            cout << P[i] << endl;
+        }
+        cout << endl;
+        for (int i = 0; i < 4; i++) {
+            cout << matrixC[i] << endl;
+        }
     }
-cout << endl;
-    for (int i = 0; i < 4; i++) {
-        cout << matrixC[i] << endl;
-    }
+    
 }
 
 int main() {
@@ -43,7 +72,7 @@ int main() {
     }
 
     // initialize matrices
-    int *matrixA = new int[matrixSize*matrixSize];
+    int *matrixA = new int[4];
     int *matrixB = new int[matrixSize*matrixSize];
     int *matrixC = new int[matrixSize*matrixSize]; //output
 
